ccac31879adadff456998a0d648ba9a4ffb9528b
occamskatana <johngallweycarter@gmail.com>
2015-12-22 17:24:45 -0500

    roles added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+         after_initialize {self.role ||= :user}
+
+         enum role: [:user, :member, :admin]
 end
---
+++ app/policies/wiki_policy.rb
@@ -0,0 +1,10 @@
+class WikiPolicy < ApplicationPolicy
+
+	def create
+		user.admin?
+	end
+
+	def update
+		user.admin?
+	end
+end
